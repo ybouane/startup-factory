c4fec927d1d19ec1e424b830356eb466b28e221d
Yassine Bouanane <yassine@bouanane.com>
2020-05-03 13:11:17 -0400

    Bug correction

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const cwd = process.cwd();
 			_SECONDARY_COLOR_	: secondaryColor,
 		};
 		for(let s in toReplace) {
-			let files;
+			let files = [];
 			try {
 				files = (await H.exec('find '+projectHandle+'/ \'!\' -path "*node_modules*" -type f "(" -name "*.js" -o -name "*.conf" -o -name "*.scss" -o -name "*.jinja" -o -name "*.json" ")" -exec grep -l "'+s+'" {} +')).stdout.trim().split(/\r?\n/);
 			} catch(e){}
