3b0aa32d5f7b6343d403b0610bcf566952c103cc
Yassine Bouanane <yassine@bouanane.com>
2020-05-05 18:05:36 -0400

    Added information on how to configure S3 CORS policy

--- init-package/config/config.js
+++ init-package/config/config.js
@@ -11,6 +11,30 @@ module.exports = {
 
 	s3Region			: 'us-east-2',
 
+	/*
+	// Set CORS policy of bucket as follows:
+	<?xml version="1.0" encoding="UTF-8"?>
+	<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+	<CORSRule>
+		<AllowedOrigin>https://sweetdata.io</AllowedOrigin>
+		<AllowedMethod>GET</AllowedMethod>
+		<AllowedMethod>PUT</AllowedMethod>
+		<AllowedMethod>POST</AllowedMethod>
+		<MaxAgeSeconds>3000</MaxAgeSeconds>
+		<ExposeHeader>ETag</ExposeHeader>
+		<AllowedHeader>Authorization</AllowedHeader>
+		<AllowedHeader>x-amz-date</AllowedHeader>
+		<AllowedHeader>x-amz-content-sha256</AllowedHeader>
+		<AllowedHeader>content-type</AllowedHeader>
+	</CORSRule>
+	<CORSRule>
+		<AllowedOrigin>*</AllowedOrigin>
+		<AllowedMethod>GET</AllowedMethod>
+		<MaxAgeSeconds>3000</MaxAgeSeconds>
+	</CORSRule>
+	</CORSConfiguration>
+	*/
+
 	uploaders			: {
 		profilePicture	: {
 			bucket			: '',
